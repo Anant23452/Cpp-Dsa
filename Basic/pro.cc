81a36aa192848dac379262ded591118300b11fc7
Anant2345 <imagine231216@gmail.com>
2024-09-25 16:43:22 +0530

    first commit

---
+++ Basic/pro.cc
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x,y;
+    cout<<"Enter the number";
+    cin>>x>>y;
+    cout<<"Sum of "<<x<<" and "<<y<<" is "<<x+y;
+    return 0;
+ 
+}
